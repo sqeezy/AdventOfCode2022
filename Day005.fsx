e9d67fee8d78d7779f2501c7992253d63b435252
Anton Herzog <info@antonherzog.com>
2022-12-05 07:43:30 +0100

    Day 5  input

---
+++ Day005.fsx
@@ -0,0 +1,33 @@
+// https://adventofcode.com/2022/day/4
+
+open System
+
+let path = $@"{__SOURCE_DIRECTORY__}\Day005.txt"
+let lines = System.IO.File.ReadAllLines path
+let splitAtNewLine (s:string) = s.Split(Environment.NewLine)
+
+let partOne lines =
+    let (upper,lower) = lines |> Array.splitAt (lines |> Array.findIndex((=) ""))
+    let initialSetup =
+     1
+    0
+
+let partTwo = ignore
+
+let testInput =  @"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"
+
+let testLines = testInput |> splitAtNewLine
+
+printfn $"Part One Test Result: {partOne testLines}"
+printfn $"Part One Result: {partOne lines}"
+
+printfn $"Part One Test Result: {partTwo testLines}"
+printfn $"Part Two Result: {partTwo lines}"
