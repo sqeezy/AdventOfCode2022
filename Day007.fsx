22ba4a1e985f809c121c4dfc18647122c5f408c3
Anton Herzog <info@antonherzog.com>
2022-12-08 08:05:18 +0100

    Add link and some tree logic

--- Day007.fsx
+++ Day007.fsx
@@ -1,4 +1,5 @@
 // https://adventofcode.com/2022/day/4
+// https://fsharpforfunandprofit.com/posts/recursive-types-and-folds/#basic-recursive-type
 // https://fsharpforfunandprofit.com/posts/recursive-types-and-folds-3b/
 
 open System
@@ -31,9 +32,39 @@ $ ls
 let path = $@"{__SOURCE_DIRECTORY__}\Day007.txt"
 let lines = System.IO.File.ReadAllLines path
 
-type Node =
+
+type Tree =
     | File of (string * int)
-    | Folder of (string * Node seq)
+    | Folder of (string * Tree seq)
+
+module Tree =
+    let rec cata fLeaf fNode tree =
+        let recurse = cata fLeaf fNode
+        match tree with
+        | File leafInfo ->
+            fLeaf leafInfo
+        | Folder (nodeInfo,subtrees) ->
+            fNode nodeInfo (subtrees |> Seq.map recurse)
+
+    let rec fold fLeaf fNode acc tree =
+        let recurse = fold fLeaf fNode
+        match tree with
+        | File leafInfo ->
+            fLeaf acc leafInfo
+        | Folder (nodeInfo,subtrees) ->
+            // determine the local accumulator at this level
+            let localAccum = fNode acc nodeInfo
+            // thread the local accumulator through all the subitems using Seq.fold
+            let finalAccum = subtrees |> Seq.fold recurse localAccum
+            // ... and return it
+            finalAccum
+
+let smallTree = Folder("/", seq {File("boot.txt", 100); Folder("usr", seq {File("mng.txt", 150)})})
+
+Tree.cata 
+    (fun (name, size) -> (name, size))
+    (fun name sub -> (name, Seq.sum (sub|>Seq.map snd)))
+     smallTree
 
 let partOne = ignore
 let partTwo = ignore
